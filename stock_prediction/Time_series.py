d9b87e4e5b756c7eff3e2b3459a2d47da784c7dd
Quoc Thinh Vo <72519491+qtvo93@users.noreply.github.com>
2021-04-26 18:01:04 -0400

    time series model

---
+++ stock_prediction/Time_series.py
@@ -0,0 +1,203 @@
+"""
+Sample API stock price predictions
+
+Author: Quoc Thinh Vo
+Last Edited: 04/ 05/ 2021
+
+"""
+from datetime import datetime, timedelta
+import pandas as pd
+from tqdm import tqdm
+import time
+
+import alpaca_trade_api as tradeapi
+
+def break_period_in_dates_list(start_date, end_date, days_per_step):
+    '''Break period between start_date and end_date in steps of days_per_step days.'''
+    step_start_date = start_date
+    delta = timedelta(days=days_per_step)
+    dates_list = []
+    while end_date > (step_start_date + delta):
+        dates_list.append((step_start_date, step_start_date+delta))
+        step_start_date += delta
+    dates_list.append((step_start_date, end_date))
+    return dates_list
+
+
+def format_timestep_list(timestep_list):
+    '''Format dates in ISO format plus timezone. Note that first day starts at 00:00 and last day ends at 23:00hs.'''
+    for i, d in enumerate(timestep_list):
+        timestep_list[i] = (d[0].isoformat() + '-04:00', (d[1].isoformat().split('T')[0] + 'T23:00:00-04:00'))
+    return timestep_list
+
+
+def get_df_from_barset(barset):
+    '''Create a Pandas Dataframe from a barset.'''
+    df_rows = []
+    for symbol, bar in barset.items():
+        rows = bar.__dict__.get('_raw')
+        for i, row in enumerate(rows):
+            row['symbol'] = symbol
+        df_rows.extend(rows)
+
+    return pd.DataFrame(df_rows)
+
+
+def download_data(aps, symbols, start_date, end_date, filename='data.csv'):
+    '''Download data from REST manager for list of symbols, from start_date at 00:00hs to end_date at 23:00hs,
+    and save it to filename as a csv file.'''
+    timesteps = format_timestep_list(break_period_in_dates_list(start_date, end_date, 10))
+    df = pd.DataFrame()
+    
+    for timestep in tqdm(timesteps):
+        barset = aps.get_barset(symbols, '5Min', limit=1000, start=timestep[0], end=timestep[1])
+        df = df.append(get_df_from_barset(barset))
+        time.sleep(0.1)
+        
+    df.to_csv(filename)
+    
+    
+# Call method.
+user_input=input("Which stock would you like to get predictions?:")
+aps = tradeapi.REST(key_id = 'PKPVKNF9PIEBCDYE54LC', 
+                    secret_key = 's8ppa683lYuePmyclqmZdOA2siukGrQDs7psEqyd',
+                    base_url = 'https://paper-api.alpaca.markets')
+
+download_data(aps        = aps, 
+              symbols    = [user_input,], 
+              start_date = datetime.strptime('01/01/20', '%d/%m/%y'), 
+              end_date   = datetime.strptime('01/02/21', '%d/%m/%y'),
+              filename   = 'test_time_series.csv')
+
+#%%%
+
+#Processing data now
+
+import pickle
+import pandas as pd
+import numpy as np
+
+with open('test_time_series.csv', 'rb') as f:
+    df= pd.read_csv(f)    
+DATA = df
+
+#X_data = DATA.drop(columns=['c','symbol']).to_numpy()
+
+y_data = DATA['c'].copy()
+
+#%%
+import os
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use('fivethirtyeight')
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import math
+import numpy as np
+
+#Test for staionarity
+def test_stationarity(timeseries):
+    #Determing rolling statistics
+    rolmean = timeseries.rolling(12).mean()
+    rolstd = timeseries.rolling(12).std()
+    #Plot rolling statistics:
+    plt.plot(timeseries, color='blue',label='Original')
+    plt.plot(rolmean, color='red', label='Rolling Mean')
+    plt.plot(rolstd, color='black', label = 'Rolling Std')
+    plt.legend(loc='best')
+    plt.title('Rolling Mean and Standard Deviation')
+    plt.show(block=False)
+    
+    print("Results of dickey fuller test")
+    adft = adfuller(timeseries,autolag='AIC')
+    # output for dft will give us without defining what the values are.
+    #hence we manually write what values does it explains using a for loop
+    output = pd.Series(adft[0:4],index=['Test Statistics','p-value','No. of lags used','Number of observations used'])
+    for key,values in adft[4].items():
+        output['critical value (%s)'%key] =  values
+    print(output)
+    
+test_stationarity(y_data)
+
+#%%
+result = seasonal_decompose(y_data, model='multiplicative', freq = 30)
+fig = plt.figure()  
+fig = result.plot()  
+fig.set_size_inches(16, 9)
+#%%
+from pylab import rcParams
+rcParams['figure.figsize'] = 10, 6
+df_log = np.log(y_data)
+moving_avg = df_log.rolling(12).mean()
+std_dev = df_log.rolling(12).std()
+plt.legend(loc='best')
+plt.title('Moving Average')
+plt.plot(std_dev, color ="black", label = "Standard Deviation")
+plt.plot(moving_avg, color="red", label = "Mean")
+plt.legend()
+plt.show()
+
+#%%#split data into train and training set
+train_data, test_data = df_log[3:int(len(df_log)*0.9)], df_log[int(len(df_log)*0.9):]
+plt.figure(figsize=(10,6))
+plt.grid(True)
+plt.xlabel('Dates')
+plt.ylabel('Closing Prices')
+plt.plot(df_log, 'green', label='Train data')
+plt.plot(test_data, 'blue', label='Test data')
+plt.legend()
+
+#%%
+
+model_autoARIMA = auto_arima(train_data, start_p=0, start_q=0,
+                      test='adf',       # use adftest to find             optimal 'd'
+                      max_p=3, max_q=3, # maximum p and q
+                      m=1,              # frequency of series
+                      d=None,           # let model determine 'd'
+                      seasonal=False,   # No Seasonality
+                      start_P=0, 
+                      D=0, 
+                      trace=True,
+                      error_action='ignore',  suppress_warnings=True, stepwise=True)
+print(model_autoARIMA.summary())
+
+model_autoARIMA.plot_diagnostics(figsize=(15,8))
+plt.show()
+
+model = ARIMA(train_data, order=(3, 1, 2))  
+fitted = model.fit(disp=-1)  
+print(fitted.summary())
+#%%
+# Forecast
+fc, se, conf = fitted.forecast(int(test_data.shape[0]), alpha=0.05)  # 95% confidence
+fc_series = pd.Series(fc, index=test_data.index)
+lower_series = pd.Series(conf[:, 0], index=test_data.index)
+upper_series = pd.Series(conf[:, 1], index=test_data.index)
+plt.figure(figsize=(12,5), dpi=100)
+plt.plot(train_data, label='training')
+plt.plot(test_data, color = 'blue', label='Actual Stock Price')
+plt.plot(fc_series, color = 'orange',label='Predicted Stock Price')
+plt.fill_between(lower_series.index, lower_series, upper_series, 
+                 color='k', alpha=.10)
+plt.title('Stock Price Prediction')
+plt.xlabel('Time')
+plt.ylabel('Actual Stock Price')
+plt.legend(loc='upper left', fontsize=8)
+plt.show()
+## report performance
+mse = mean_squared_error(test_data, fc)
+print('MSE: '+str(mse))
+mae = mean_absolute_error(test_data, fc)
+print('MAE: '+str(mae))
+rmse = math.sqrt(mean_squared_error(test_data, fc))
+print('RMSE: '+str(rmse))
+mape = np.mean(np.abs(fc - test_data)/np.abs(test_data))
+print('MAPE: '+str(mape))
